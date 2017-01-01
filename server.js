85b0b6db40d851b85507632ec1c0d8de47f9cc19
Kent <kcponline@outlook.com>
2017-01-01 01:12:57 -0800

    test heroku mlab config variable

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use(express.static('./public'));
 // -------------------------------------------------
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect('mongodb://heroku_xkvq4cfq:egppcbpinecs7jt0rvhvbs45jg@ds151008.mlab.com:51008/heroku_xkvq4cfq');
+// mongoose.connect('mongodb://heroku_xkvq4cfq:egppcbpinecs7jt0rvhvbs45jg@ds151008.mlab.com:51008/heroku_xkvq4cfq');
+mongoose.connect('mongodb://MONGODB_URI');
 var db = mongoose.connection;
 
 db.on('error', function (err) {
