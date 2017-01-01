f212a687724994c3392c4ab60a801a4d24108672
Kent <kcponline@outlook.com>
2017-01-01 00:57:44 -0800

    setup mlab and add mlab key to server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static('./public'));
 // -------------------------------------------------
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect('mongodb://localhost');
+mongoose.connect('mongodb://heroku_xkvq4cfq:egppcbpinecs7jt0rvhvbs45jg@ds151008.mlab.com:51008/heroku_xkvq4cfq');
 var db = mongoose.connection;
 
 db.on('error', function (err) {
